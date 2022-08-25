e88b65008c5fddb480262c7d20b5e10fc2d768a7
Rohit Singh <95858652+rohitsinghrajpoot@users.noreply.github.com>
2022-08-25 20:39:54 +0530

    Add files via upload

---
+++ register/src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <form className='login_form'>
+          <h1>singn up here</h1>
+        <input type='name' placeholder='Name'></input>
+        <input type='email' placeholder='Email'></input>
+        <input type="password" placeholder='Password'>Password</input>
+      </form>
+    </div>
+  )
+}
+
+export default Login
